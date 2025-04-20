5fb860a1386efab218be4ba8b38549c5591bf43d
Harshitha K <harshitha02012005@gmail.com>
2025-04-20 21:03:52 +0530

    Time: 1 ms (99.56%), Space: 41.4 MB (96.71%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,7 +2,6 @@ class Solution {
     public boolean isValid(String s) {
         char ch[]=new char[s.length()];
         int top=-1;
-
         for(int i=0;i<s.length();i++){
             if(top!=-1 && check(ch[top],s.charAt(i))){
                 top--;
@@ -24,5 +23,5 @@ class Solution {
             if(inside=='{' && outside=='}')
             return true;
             return false;
-        }
+    }
 }
