394b97cbf5162d3c2d589c2f32fea2825a371394
Harshitha K <harshitha02012005@gmail.com>
2025-04-20 17:04:41 +0530

    Time: 0 ms (100%), Space: 40.5 MB (91.08%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -2,9 +2,9 @@ class Solution {
     public int trailingZeroes(int n) {
         int sum=0;
         while(n>1){
-    n=n/5;
-    sum=sum+n;
-    }
-    return sum;
+            n/=5;
+            sum+=n;
+        }
+        return sum;
     }
 }
