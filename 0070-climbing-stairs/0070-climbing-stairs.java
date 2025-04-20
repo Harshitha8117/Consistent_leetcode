631913caadcda81232309904331c5f01d4fc6814
Harshitha K <harshitha02012005@gmail.com>
2025-04-20 21:08:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,12 +1,11 @@
 class Solution {
     public int climbStairs(int n) {
-        int temp=0;
         int a=0,b=1;
         for(int i=0;i<n;i++){
-            temp=a+b;
+            int temp=a+b;
             a=b;
             b=temp;
-        }    
-        return temp;
+        }
+        return a;
     }
 }
