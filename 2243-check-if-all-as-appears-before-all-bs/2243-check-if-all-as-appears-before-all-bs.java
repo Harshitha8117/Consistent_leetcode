5c0e38a4161bae679d13905f6e395d31e085d74e
Harshitha K <harshitha02012005@gmail.com>
2025-04-23 20:30:28 +0530

    Time: 1 ms (17.36%), Space: 41.7 MB (52.09%) - LeetHub

--- 2243-check-if-all-as-appears-before-all-bs/2243-check-if-all-as-appears-before-all-bs.java
+++ 2243-check-if-all-as-appears-before-all-bs/2243-check-if-all-as-appears-before-all-bs.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean checkString(String s) {
-        char a[]=s.toCharArray();
         int count=0;
+        char a[]=s.toCharArray();
         for(int i=0;i<s.length()-1;i++){
-            if(a[i]=='b'){
-                if(a[i+1]=='a'){
-                    count=-1;
-                }
+        if(a[i]=='b'){
+            if(a[i+1]=='a'){
+                count=-1;
             }
+        }
         }
         if(count==0)
         return true;
