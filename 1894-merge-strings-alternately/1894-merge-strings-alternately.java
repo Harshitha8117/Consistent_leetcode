6fc6777acabbf470dec44a9afed4536a2902024e
Harshitha K <harshitha02012005@gmail.com>
2025-04-23 21:39:54 +0530

    Time: 6 ms (21.99%), Space: 43.2 MB (5.6%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -2,7 +2,7 @@ class Solution {
     public String mergeAlternately(String word1, String word2) {
         int count=0;
         String word="";
-        while(count<word1.length()||count<word2.length()){
+        while(count<word1.length() || count<word2.length()){
             if(count<word1.length()){
                 word+=word1.charAt(count);
             }
@@ -14,3 +14,5 @@ class Solution {
         return word;
     }
 }
+
+       
