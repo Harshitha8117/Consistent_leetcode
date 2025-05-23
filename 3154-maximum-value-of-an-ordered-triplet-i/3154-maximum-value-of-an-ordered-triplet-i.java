fde8a9470153f6b915f56f2c8caf060e45686cc5
Harshitha K <harshitha02012005@gmail.com>
2025-05-23 11:41:31 +0530

    Time: 1 ms (98.69%), Space: 41.9 MB (75.85%) - LeetHub

--- 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.java
+++ 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.java
@@ -1,11 +1,11 @@
 class Solution {
     public long maximumTripletValue(int[] nums) {
-        long maxTriplet = 0, maxElement = 0, maxDiff = 0;
-        for (int num : nums) {
-            maxTriplet = Math.max(maxTriplet, maxDiff * num);
-            maxDiff = Math.max(maxDiff, maxElement - num);
-            maxElement = Math.max(maxElement, num);
+        long maxTriplets=0,maxNums=0,maxDiff=0;
+        for(int num:nums){
+            maxTriplets=Math.max(maxTriplets,maxDiff*num);
+            maxDiff=Math.max(maxDiff,maxNums-num);
+            maxNums=Math.max(maxNums,num);
         }
-        return maxTriplet;
+        return maxTriplets;
     }
 }
