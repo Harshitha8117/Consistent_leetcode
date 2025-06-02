b8f717f0f53010cbdaa5414e7cd7641b77f26946
Harshitha K <harshitha02012005@gmail.com>
2025-06-02 21:44:23 +0530

    Time: 2 ms (99.56%), Space: 46.6 MB (6.03%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,29 +1,19 @@
 class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
+        int c = 0;
         int[] candies = new int[n];
-        Arrays.fill(candies, 1);
-
-        // Left to Right
-        for (int i = 1; i < n; i++) {
-            if (ratings[i] > ratings[i - 1]) {
-                candies[i] = candies[i - 1] + 1;
-            }
+        for (int i=0;i<n;i++) 
+        candies[i] = 1;
+        for (int i=1;i<n;i++){
+            if (ratings[i] > ratings[i-1])
+                candies[i] = candies[i-1] + 1;
         }
-
-        // Right to Left
-        for (int i = n - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1]) {
-                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
-            }
+        for (int i=n-1; i>0; i--) {
+            if (ratings[i-1] > ratings[i])
+                candies[i-1] = Math.max(candies[i] + 1, candies[i - 1]);
+            c += candies[i-1];
         }
-
-        // Total
-        int total = 0;
-        for (int c : candies) {
-            total += c;
-        }
-
-        return total;
+        return c + candies[n-1];
     }
 }
