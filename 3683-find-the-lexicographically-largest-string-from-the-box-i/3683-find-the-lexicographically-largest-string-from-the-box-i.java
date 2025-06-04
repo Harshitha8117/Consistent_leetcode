2e71b868ae9326f4825e7fbc16cc2ba06ef3662d
Harshitha K <harshitha02012005@gmail.com>
2025-06-04 06:25:03 +0530

    Time: 4 ms (87.96%), Space: 45.5 MB (42.59%) - LeetHub

---
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.java
@@ -0,0 +1,44 @@
+class Solution
+{
+    public String answerString(String word, int numFriends)
+    {
+        int N = word.length();
+
+        // Step 1: Handle base case where all friends can split the string
+        if (numFriends == 1)
+        {
+            return word;
+        }
+        char c = 'a';
+        for (char ch : word.toCharArray())
+        {
+            if (ch > c)
+            {
+                c = ch;
+            }
+        }
+
+        String maxString = ""; 
+        for (int pos = 0; pos < N; pos++)
+        {
+            if (word.charAt(pos) == c)
+            {
+                int lAllowed = Math.min(N - pos, N - numFriends + 1);
+
+                if (lAllowed <= 0)
+                {
+                    continue;
+                }
+
+                String s = word.substring(pos, pos + lAllowed);
+                if (s.compareTo(maxString) > 0)
+                {
+                    maxString = s;
+                }
+            }
+        }
+
+        // Step 6: Return the result
+        return maxString;
+    }
+}
