2dd1b19f5aa6c91ae2f28ac1a19e6dc87402672c
Harshitha K <harshitha02012005@gmail.com>
2025-07-17 20:24:52 +0530

    Time: 19 ms (9.85%), Space: 40.7 MB (84.18%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,7 +1,7 @@
 class Solution {
     public int mySqrt(int x) {
-        long i=1;
+        long i=0;
         for(i=1;(i*i)<=x;i++);
-        return (int)i-1;
+            return (int)i-1;
     }
 }
