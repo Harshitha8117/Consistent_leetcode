9c07baef928403112607750bea8e1ac06cb00497
Harshitha K <harshitha02012005@gmail.com>
2025-07-25 06:22:11 +0530

    Time: 3 ms (97.8%), Space: 44.8 MB (10.4%) - LeetHub

--- 1210-mean-of-array-after-removing-some-elements/1210-mean-of-array-after-removing-some-elements.java
+++ 1210-mean-of-array-after-removing-some-elements/1210-mean-of-array-after-removing-some-elements.java
@@ -2,11 +2,11 @@ class Solution {
     public double trimMean(int[] arr) {
         double sum=0;
         Arrays.sort(arr);
-        int percent=(int)(arr.length*0.05);
-        for(int i=percent;i<(arr.length-percent);i++){
+        int res=(int)(arr.length*0.05);
+        for(int i=res;i<(arr.length-res);i++){
             sum+=arr[i];
         }
-        return sum/(arr.length-percent*2);
+        return sum/(arr.length-res*2);
     }
 }
 
