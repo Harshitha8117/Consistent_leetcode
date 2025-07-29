7dd7852e1e47e15774b32dcf6d81138f2948e8b3
Harshitha K <harshitha02012005@gmail.com>
2025-07-29 06:26:30 +0530

    Time: 1 ms (95.99%), Space: 45.4 MB (47.3%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.java
+++ 2058-concatenation-of-array/2058-concatenation-of-array.java
@@ -1,5 +1,17 @@
 class Solution {
     public int[] getConcatenation(int[] nums) {
+        int[] a=new int[nums.length*2];
+        for(int i=0;i<nums.length;i++){
+            a[i]=nums[i];
+            a[i+nums.length]=nums[i];
+        }
+        return a;
+    }
+}
+
+
+/*class Solution {
+    public int[] getConcatenation(int[] nums) {
         int size=nums.length*2;
         int ans[]=new int[size];
         int k=0;
@@ -12,4 +24,6 @@ class Solution {
         }
         return ans;
     }
-}
+}*/
+
+
