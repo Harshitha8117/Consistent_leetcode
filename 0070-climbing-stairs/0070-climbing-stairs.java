4c5c3e29d5afd83e45f99f821b0ec789977c4687
Harshitha K <harshitha02012005@gmail.com>
2025-08-17 15:48:02 +0530

    Time: 0 ms (100%), Space: 41 MB (5.34%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,4 +1,20 @@
-class Solution {
+class Solution{
+    public int climbStairs(int n){
+        if(n<2)
+        return 1;
+        int a=1,b=1;
+        for(int i=2;i<=n;i++){
+            int temp=b;
+            b=a+b;
+            a=temp;
+        }
+        return b;
+    }
+}
+
+
+
+/*class Solution {
     public int climbStairs(int n) {
         if (n == 0 || n == 1) {
             return 1;
@@ -10,4 +26,4 @@ class Solution {
         }
         return dp[n];
     }
-}
+}*/
