3493b145d99900aa49a7e8079a1994735fd07215
Harshitha K <harshitha02012005@gmail.com>
2025-08-18 20:00:59 +0530

    Time: 2 ms (96.55%), Space: 42.4 MB (6.05%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public boolean isValid(String s) {
         char ch[]=new char[s.length()];
         int top=-1;
@@ -23,5 +23,28 @@ class Solution {
             if(inside=='{' && outside=='}')
             return true;
             return false;
+    }
+}*/
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } else {
+                if (st.isEmpty())
+                return false;
+                char top = st.peek();
+                if ((c == ')' && top == '(') ||
+                    (c == '}' && top == '{') ||
+                    (c == ']' && top == '[')) {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
     }
 }
