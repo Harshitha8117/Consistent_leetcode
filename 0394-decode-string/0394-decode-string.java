bae7ffd230caff10efe788ac9f9ae27a415da369
Harshitha K <harshitha02012005@gmail.com>
2025-08-18 19:59:21 +0530

    Time: 1 ms (70.28%), Space: 41.5 MB (82.34%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public String decodeString(String s) {
         Stack<Integer> dig=new Stack<>();
         Stack <StringBuilder> ch=new Stack<>();
@@ -19,6 +19,40 @@ class Solution {
                 dec=ch.pop();
                 int po=dig.pop();
                 for(int i=0;i<po;i++){
+                    dec.append(sb);
+                }
+                sb=dec;
+            }
+            else{
+                sb.append(chi);
+            }
+        }
+        return sb.toString();
+    }
+}*/
+
+
+class Solution {
+    public String decodeString(String s) {
+        Stack<Integer> dig=new Stack<>();
+        Stack<StringBuilder> ch=new Stack<>();
+        StringBuilder sb=new StringBuilder();
+        int k=0;
+        for(char chi:s.toCharArray()){
+            if(Character.isDigit(chi)){
+                k=k*10+(chi-'0');
+            }
+            else if(chi=='['){
+                dig.push(k);
+                ch.push(sb);
+                sb=new StringBuilder();
+                k=0;
+            }
+            else if(chi==']'){
+                StringBuilder dec=new StringBuilder();
+                dec=ch.pop();
+                int g=dig.pop();
+                for(int i=0;i<g;i++){
                     dec.append(sb);
                 }
                 sb=dec;
