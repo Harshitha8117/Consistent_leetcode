2b24a488a673c74b2931cc2f6307bdcc959d20c5
Harshitha K <harshitha02012005@gmail.com>
2025-08-24 15:24:30 +0530

    Time: 14 ms (7.1%), Space: 44.6 MB (80.52%) - LeetHub

--- 2283-sort-even-and-odd-indices-independently/2283-sort-even-and-odd-indices-independently.java
+++ 2283-sort-even-and-odd-indices-independently/2283-sort-even-and-odd-indices-independently.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int[] sortEvenOdd(int[] nums) {
         List<Integer> odd=new ArrayList<>();
         List<Integer> even=new ArrayList<>();
@@ -22,6 +22,57 @@ class Solution {
                 nums[i]=odd.get(o++);
             }
         }
+        return nums;
+    }
+}*/
+
+
+class Solution {
+    public int[] sortEvenOdd(int[] nums) {
+        List<Integer> even = new ArrayList<>();
+        List<Integer> odd = new ArrayList<>();
+
+        // Separate even and odd indexed elements
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0) {
+                even.add(nums[i]);
+            } else {
+                odd.add(nums[i]);
+            }
+        }
+
+        // Sort even list in ascending order
+        for (int i = 0; i < even.size(); i++) {
+            for (int j = i + 1; j < even.size(); j++) {
+                if (even.get(i) > even.get(j)) {
+                    int temp = even.get(i);
+                    even.set(i, even.get(j));
+                    even.set(j, temp);
+                }
+            }
+        }
+
+        // Sort odd list in descending order
+        for (int i = 0; i < odd.size(); i++) {
+            for (int j = i + 1; j < odd.size(); j++) {
+                if (odd.get(i) < odd.get(j)) {
+                    int temp = odd.get(i);
+                    odd.set(i, odd.get(j));
+                    odd.set(j, temp);
+                }
+            }
+        }
+
+        // Merge back into nums
+        int e = 0, o = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0) {
+                nums[i] = even.get(e++);
+            } else {
+                nums[i] = odd.get(o++);
+            }
+        }
+
         return nums;
     }
 }
