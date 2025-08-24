c8be8ae91bade239731f3621b817a29cb85fd8eb
Harshitha K <harshitha02012005@gmail.com>
2025-08-24 16:17:26 +0530

    Time: 0 ms (100%), Space: 41.5 MB (81.81%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -3,7 +3,6 @@ class Solution {
         List<Integer> sol=new ArrayList<>();
         int top=0,bottom=matrix.length-1;
         int left=0,right=matrix[0].length-1;
-
         while(top<=bottom && left<=right){
             for(int i=left;i<=right;i++){
                 sol.add(matrix[top][i]);
@@ -14,18 +13,19 @@ class Solution {
             }
             right--;
             if(top<=bottom){
-                for(int i=right;i>=left;i--){
-                    sol.add(matrix[bottom][i]);
-                }
-                bottom--;
+            for(int i=right;i>=left;i--){
+                sol.add(matrix[bottom][i]);
+            }
+            bottom--;
             }
             if(left<=right){
-                for(int i=bottom;i>=top;i--){
-                    sol.add(matrix[i][left]);
-                }
-                left++;
+            for(int i=bottom;i>=top;i--){
+                sol.add(matrix[i][left]);
+            }
+            left++;
             }
         }
         return sol;
     }
 }
+
