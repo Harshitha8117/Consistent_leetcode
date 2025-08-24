4e86f65e5447fdcb8e2276a9191d0b41e2d999ff
Harshitha K <harshitha02012005@gmail.com>
2025-08-24 16:43:08 +0530

    Time: 1 ms (70.35%), Space: 41.5 MB (68.28%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,64 +1,30 @@
-/*class Solution {
-    public String decodeString(String s) {
-        Stack<Integer> dig=new Stack<>();
-        Stack <StringBuilder> ch=new Stack<>();
-        StringBuilder sb=new StringBuilder();
-        int k=0;
-        for(char chi:s.toCharArray()){
-            if(Character.isDigit(chi)){
-                k=k*10+(chi-'0');
-            }
-            else if(chi=='['){
-                dig.push(k);
-                ch.push(sb);
-                sb=new StringBuilder();
-                k=0;
-            }
-            else if(chi==']'){
-                StringBuilder dec=new StringBuilder();
-                dec=ch.pop();
-                int po=dig.pop();
-                for(int i=0;i<po;i++){
-                    dec.append(sb);
-                }
-                sb=dec;
-            }
-            else{
-                sb.append(chi);
-            }
-        }
-        return sb.toString();
-    }
-}*/
-
-
 class Solution {
     public String decodeString(String s) {
         Stack<Integer> dig=new Stack<>();
         Stack<StringBuilder> ch=new Stack<>();
         StringBuilder sb=new StringBuilder();
         int k=0;
-        for(char chi:s.toCharArray()){
-            if(Character.isDigit(chi)){
-                k=k*10+(chi-'0');
+        for(char num:s.toCharArray()){
+            if(Character.isDigit(num)){
+                k=k*10+(num-'0');
             }
-            else if(chi=='['){
+            else if(num=='['){
                 dig.push(k);
                 ch.push(sb);
                 sb=new StringBuilder();
                 k=0;
             }
-            else if(chi==']'){
-                StringBuilder dec=new StringBuilder();
-                dec=ch.pop();
-                int g=dig.pop();
-                for(int i=0;i<g;i++){
-                    dec.append(sb);
+            else if(num==']'){
+                StringBuilder str=new StringBuilder();
+                str=ch.pop();
+                int n=dig.pop();
+                for(int i=0;i<n;i++){
+                    str.append(sb);
                 }
-                sb=dec;
+                sb=str;
             }
             else{
-                sb.append(chi);
+                sb.append(num);
             }
         }
         return sb.toString();
