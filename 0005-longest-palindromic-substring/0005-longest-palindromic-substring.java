71f1e4e7001b3aad3629cc09f362748d4c984eda
Harshitha K <harshitha02012005@gmail.com>
2025-08-24 14:56:59 +0530

    Time: 219 ms (23.48%), Space: 46.8 MB (7.23%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public String longestPalindrome(String s) {
         int n=s.length();
         int start=0,end=1,max=0;
@@ -16,5 +16,27 @@ class Solution {
             }
         }
         return s.substring(start,end);     
+    }
+}
+*/
+
+class Solution{
+    public String longestPalindrome(String s){
+        int n=s.length();
+        int start=0,end=1,maxLen=0;
+        boolean dp[][]=new boolean[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(s.charAt(i)==s.charAt(j) && ((i-j<2)||dp[i-1][j+1])){
+                    dp[i][j]=true;
+                    if(i-j>maxLen){
+                        maxLen=i-j;
+                        start=j;
+                        end=i+1;
+                    }
+                }
+            }
+        }
+        return s.substring(start,end);
     }
 }
