58a297672525d9585064d7e2fdb38225f4471987
Harshitha K <harshitha02012005@gmail.com>
2025-08-24 15:31:35 +0530

    Time: 12 ms (8.88%), Space: 45 MB (6%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public String largestNumber(int[] nums) {
         String[] strarr = new String[nums.length];
         for(int i = 0; i<nums.length; i++){
@@ -14,5 +14,42 @@ class Solution {
         }
       
       return sb.toString();
+    }
+}*/
+
+class Solution {
+    public String largestNumber(int[] nums) {
+        // Convert integers to strings manually
+        String[] strNums = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            strNums[i] = "" + nums[i];
+        }
+
+        // Manual sort using custom comparison logic
+        for (int i = 0; i < strNums.length; i++) {
+            for (int j = i + 1; j < strNums.length; j++) {
+                String ij = strNums[i] + strNums[j];
+                String ji = strNums[j] + strNums[i];
+                if (ji.compareTo(ij) > 0) {
+                    // Swap
+                    String temp = strNums[i];
+                    strNums[i] = strNums[j];
+                    strNums[j] = temp;
+                }
+            }
+        }
+
+        // Edge case: if the largest number is "0", return "0"
+        if (strNums[0].equals("0")) {
+            return "0";
+        }
+
+        // Concatenate manually
+        String result = "";
+        for (int i = 0; i < strNums.length; i++) {
+            result += strNums[i];
+        }
+
+        return result;
     }
 }
