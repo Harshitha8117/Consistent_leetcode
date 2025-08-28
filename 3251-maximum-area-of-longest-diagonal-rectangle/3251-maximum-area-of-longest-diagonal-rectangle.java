f082301a2bb38501e143ca1d59df238e0ba8e5b7
Harshitha K <harshitha02012005@gmail.com>
2025-08-28 06:10:32 +0530

    Time: 1 ms (92.66%), Space: 44.5 MB (56.48%) - LeetHub

--- 3251-maximum-area-of-longest-diagonal-rectangle/3251-maximum-area-of-longest-diagonal-rectangle.java
+++ 3251-maximum-area-of-longest-diagonal-rectangle/3251-maximum-area-of-longest-diagonal-rectangle.java
@@ -3,14 +3,14 @@ class Solution {
         double max=-1;
         int sol=0;
         for(int a[]:dimensions){
-            int l=a[0],b=a[1];
-            double dimen=Math.sqrt((double)l*l+(double)b*b);
-            if(dimen>max){
-                max=dimen;
-                sol=l*b;
+            int len=a[0],b=a[1];
+            double peri=Math.sqrt((double)len*len+(double)b*b);
+            if(peri>max){
+                max=peri;
+                sol=len*b;
             }
-            else if(dimen==max){
-                sol=Math.max(sol,l*b);
+            else if(peri==max){
+                sol=Math.max(sol,len*b);
             }
         }
         return sol;
