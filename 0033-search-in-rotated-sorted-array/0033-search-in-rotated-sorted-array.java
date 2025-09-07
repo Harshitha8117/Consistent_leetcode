54ea3fda59e46d72d4a30f76f9401439bc7d6cc7
Harshitha K <harshitha02012005@gmail.com>
2025-09-07 13:49:17 +0530

    Time: 0 ms (100%), Space: 41.8 MB (97.69%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,4 +1,57 @@
-class Solution {
+/*class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1; 
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
+*/
+class Solution{
+    public int search(int nums[],int target){
+        int left=0,right=nums.length-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target)
+            return mid;
+            if(nums[left]<=nums[mid]){
+                if(nums[left]<=target && nums[mid]>target)
+                right=mid-1;
+                else
+                left=mid+1;
+            }
+            else{
+                if(nums[mid]<target && nums[right]>=target)
+                left=mid+1;
+                else
+                right=mid-1;
+            }
+        }
+        return -1;
+    }
+}
+
+
+
+
+
+/*class Solution {
     public int search(int[] nums, int target) {
         for(int i=0;i<nums.length;i++){
             if(nums[i]==target)
@@ -6,4 +59,4 @@ class Solution {
         }
         return -1;
     }
-}
+}*/
