31c20e8d40c61fd87d225dd0a9af275c92e356be
Harshitha K <harshitha02012005@gmail.com>
2025-09-07 14:05:49 +0530

    Time: 0 ms (100%), Space: 42.1 MB (68.89%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,37 +1,13 @@
-/*class Solution {
-    public int search(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) return mid;
-            if (nums[left] <= nums[mid]) {
-                if (nums[left] <= target && target < nums[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1; 
-                }
-            } else {
-                if (nums[mid] < target && target <= nums[right]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
-            }
-        }
-        return -1;
-    }
-}
-*/
 class Solution{
-    public int search(int nums[],int target){
+    public int search(int[] nums,int target){
         int left=0,right=nums.length-1;
         while(left<=right){
             int mid=left+(right-left)/2;
-            if(nums[mid]==target)
-            return mid;
+            if(nums[mid]==target){
+                return mid;
+            }
             if(nums[left]<=nums[mid]){
-                if(nums[left]<=target && nums[mid]>target)
+                if(nums[left]<=target && target<nums[mid])
                 right=mid-1;
                 else
                 left=mid+1;
@@ -46,9 +22,6 @@ class Solution{
         return -1;
     }
 }
-
-
-
 
 
 /*class Solution {
