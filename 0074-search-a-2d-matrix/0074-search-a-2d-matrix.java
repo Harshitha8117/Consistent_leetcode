409e47c0c37d8ad36961494492800f4ca97e3df8
Harshitha K <harshitha02012005@gmail.com>
2025-09-13 21:25:51 +0530

    Time: 0 ms (100%), Space: 42.5 MB (18.07%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -10,3 +10,28 @@ class Solution {
         return false;
     }
 }
+
+
+/*class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int left=0,right=m*n-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            int row=mid/n;
+            int col=mid%n;
+            int midd=matrix[row][col];
+            if(midd==target){
+                return true;
+            }
+            else if(midd>target){
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
+            }
+        }
+        return false;
+    }
+}*/
