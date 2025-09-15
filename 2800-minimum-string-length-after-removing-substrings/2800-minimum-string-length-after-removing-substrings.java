51bd5f71b8e08d81391ad77b2bc8e8681743c11c
Harshitha K <harshitha02012005@gmail.com>
2025-09-15 06:54:11 +0530

    Time: 1 ms (100%), Space: 43.4 MB (69.27%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
@@ -2,12 +2,12 @@ class Solution {
     public int minLength(String s) {
         char[] stack=new char[s.length()];
         int top=-1;
-        for(int i=0;i<s.length();i++){
+        for(int i=0;i<stack.length;i++){
             char ch=s.charAt(i);
             if(top>=0 && stack[top]=='A' && ch=='B'){
                 top--;
             }
-            else if(top>=0 && stack[top]=='C'&& ch=='D'){
+            else if(top>=0 && stack[top]=='C' && ch=='D'){
                 top--;
             }
             else{
@@ -15,18 +15,5 @@ class Solution {
             }
         }
         return top+1;
-
-
-
-
-        /*while(s.contains("AB")||(s.contains("CD"))){
-            if(s.contains("AB")){
-                s=s.replace("AB","");
-            }
-            if(s.contains("CD")){
-                s=s.replace("CD","");
-            }
-        }
-        return s.length();*/
     }
 }
