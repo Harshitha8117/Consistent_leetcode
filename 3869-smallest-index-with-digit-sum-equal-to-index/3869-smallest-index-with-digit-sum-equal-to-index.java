7b28840151ebd855331147e11faf398d8158e51e
Harshitha K <harshitha02012005@gmail.com>
2025-09-16 21:10:46 +0530

    Time: 1 ms (100%), Space: 44.4 MB (46.82%) - LeetHub

--- 3869-smallest-index-with-digit-sum-equal-to-index/3869-smallest-index-with-digit-sum-equal-to-index.java
+++ 3869-smallest-index-with-digit-sum-equal-to-index/3869-smallest-index-with-digit-sum-equal-to-index.java
@@ -1,4 +1,30 @@
-class Solution {
+class Solution{
+    public static int smallestIndex(int[] nums){
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(sumDigit(nums[i])==i){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int sumDigit(int n){
+        int sum=0;
+        while(n>0){
+            int temp=n%10;
+            sum+=temp;
+            n/=10;
+        }
+        return sum;
+    }
+}
+
+
+
+
+
+/*class Solution {
     public int smallestIndex(int[] nums) {
         int ss=0;
         for(int i=0;i<nums.length;i++){
@@ -16,4 +42,4 @@ class Solution {
         }
         return -1;
     }
-}
+}*/
