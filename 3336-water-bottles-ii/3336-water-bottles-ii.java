66f85a6e8a427a6d528119796a6ed0e4c6099acd
Harshitha K <harshitha02012005@gmail.com>
2025-10-02 20:53:45 +0530

    Time: 0 ms (100%), Space: 41 MB (57.42%) - LeetHub

---
+++ 3336-water-bottles-ii/3336-water-bottles-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int drunk=numBottles;
+        int empty=numBottles;
+
+        while(empty>=numExchange){
+            empty-=numExchange;
+            drunk+=1;
+            empty+=1;
+            numExchange+=1;
+        }
+        return drunk;
+    }
+}
