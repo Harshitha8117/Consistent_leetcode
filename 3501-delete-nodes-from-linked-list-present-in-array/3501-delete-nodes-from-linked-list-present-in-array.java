71fbcc3b9596484ec06cd8713307afdc886c807f
Harshitha K <harshitha02012005@gmail.com>
2025-11-01 20:00:18 +0530

    Time: 20 ms (86.51%), Space: 178.2 MB (5.52%) - LeetHub

--- 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.java
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.java
@@ -17,7 +17,6 @@ class Solution {
         ListNode dummy=new ListNode(0);
         dummy.next=head;
         ListNode curr=dummy;
-
         while(curr.next!=null){
             if(set.contains(curr.next.val)){
                 curr.next=curr.next.next;
