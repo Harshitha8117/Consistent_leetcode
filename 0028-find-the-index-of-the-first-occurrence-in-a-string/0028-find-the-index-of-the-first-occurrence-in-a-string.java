503dd121c6cbe78ab27ff35185cfb9885184951d
Harshitha K <harshitha02012005@gmail.com>
2025-11-04 06:00:20 +0530

    Time: 0 ms (100%), Space: 43 MB (5.08%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,10 +1,29 @@
-class Solution {
+/*class Solution {
     public int strStr(String haystack, String needle) {
         if(needle.isEmpty()){
             return 0 ;
         }
         if(haystack.contains(needle)){
             return haystack.indexOf((needle));
+        }
+        return -1;
+    }
+}*/
+
+
+class Solution {
+    public int strStr(String h, String n) {
+        int x=h.length();
+        int y=n.length();
+        if(x<y)
+        return -1;
+        for(int i=0;i<=x-y;i++){
+            int j=0;
+            while(j<y && h.charAt(i+j)==n.charAt(j)){
+                j++;
+            }
+            if(j==n.length())
+            return i; 
         }
         return -1;
     }
