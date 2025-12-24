1609673e4497201815c25f5584742639ba168a1c
Harshitha K <harshitha02012005@gmail.com>
2025-12-24 08:34:06 +0530

    Time: 1 ms (96.39%), Space: 47.3 MB (27.1%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.java
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -3,6 +3,19 @@ class Solution {
         int ans[]=new int[nums.length*2];
         for(int i=0;i<nums.length;i++){
             ans[i]=nums[i];
+            ans[nums.length+i]=nums[i];
+        }
+        return ans;
+    }
+}
+
+
+
+/*class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int ans[]=new int[nums.length*2];
+        for(int i=0;i<nums.length;i++){
+            ans[i]=nums[i];
         }
         int k=0;
         for(int i=nums.length;i<nums.length*2;i++){
@@ -10,4 +23,4 @@ class Solution {
         }
         return ans;
     }
-}
+}*/
