ba88efbdffd87af3723c8ec9c538625ec4dafd57
Harshitha K <harshitha02012005@gmail.com>
2025-12-26 21:43:51 +0530

    Time: 2 ms (72.21%), Space: 45.3 MB (33.72%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -2,16 +2,14 @@ class Solution {
     public int[] finalPrices(int[] prices) {
         int n=prices.length;
         int ans[]=new int[n];
-        Stack<Integer> stack = new Stack<>();
         for(int i=0;i<n;i++){
             ans[i]=prices[i];
-        }
-        for (int i=0;i<n;i++) { 
-            while (!stack.isEmpty()&& prices[i]<=prices[stack.peek()]) { 
-                int a=stack.pop(); 
-                ans[a]=prices[a]-prices[i]; 
-            } 
-            stack.push(i); 
+            for(int j=i+1;j<n;j++){
+                if(prices[j]<=prices[i]){
+                    ans[i]=prices[i]-prices[j];
+                    break;
+                }
+            }
         }
         return ans;
     }
