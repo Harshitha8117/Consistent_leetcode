6c94cba3e228c32f92d28036e3f471e789914e8a
Harshitha K <harshitha02012005@gmail.com>
2026-01-02 18:29:05 +0530

    Time: 0 ms (100%), Space: 47.6 MB (25.57%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -1,5 +1,18 @@
 class Solution {
     public int repeatedNTimes(int[] nums) {
+        Set<Integer> set=new HashSet<>();
+        for(int num:nums){
+            if(set.contains(num))
+            return num;
+            set.add(num);
+        }
+        return -1;
+    }
+}
+
+/*
+class Solution {
+    public int repeatedNTimes(int[] nums) {
         Map<Integer,Integer> map=new HashMap<>();
         for(int num:nums){
             map.put(num,map.getOrDefault(num,0)+1);
@@ -11,4 +24,4 @@ class Solution {
         }
         return -1;
     }
-}
+}*/
