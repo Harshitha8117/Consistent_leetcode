b45161ee7cd978a9180adbf25ae728ee60102cfb
Harshitha K <harshitha02012005@gmail.com>
2026-01-17 18:59:05 +0530

    Time: 1556 ms (8.13%), Space: 48.3 MB (96.82%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -12,3 +12,20 @@ class Solution {
         return count;
     }
 }
+
+/*
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int sum=0,c=0;
+        map.put(0,1);
+        for(int i:nums){
+            sum+=i;
+            if(map.containsKey(sum-k)){
+               c+=map.get(sum-k); 
+            }
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        }
+        return c;
+    }
+}*/
