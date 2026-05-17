b57d2dd0713ff29b34cca8d3123dab6b532514fc
Harshitha K <harshitha02012005@gmail.com>
2026-05-17 09:49:28 +0530

    Time: 2 ms (82.42%), Space: 53 MB (28.33%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        boolean[] visit=new boolean[arr.length];
+        return dfs(arr,start,visit);
+    }
+    private boolean dfs(int arr[],int i,boolean visit[]){
+        if(i<0 || i>=arr.length || visit[i])
+        return false;
+        if(arr[i]==0)
+        return true;
+        visit[i]=true;
+        return dfs(arr,i+arr[i],visit)|| dfs(arr,i-arr[i],visit);
+    }
+}
